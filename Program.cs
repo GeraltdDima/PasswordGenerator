7331185a2d9cdac8768544b9ef5bb6bee8551b99
dima <dimalegendarypro@gmail.com>
2024-12-25 17:55:51 +0200

    PasswordGenerator

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    private static readonly Random random = new Random(); // Fix: Specify type 'Random'
+
+    public static void Main(string[] args)
+    {
+        string selector = "qwertyuiopasdfghjklzxcvbnm,./;'[]";
+
+        int randomCount = random.Next(1, 15);
+
+        string result = string.Concat(GeneratePassword(randomCount, selector));
+
+        Console.WriteLine(result);
+    }
+
+    public static IEnumerable<char> GeneratePassword(int count, string selector)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return selector[random.Next(selector.Length)];
+        }
+    }
+}
+
